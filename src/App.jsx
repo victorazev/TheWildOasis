0429c39165cf298f48c05b21d8fb37a7dc156d8f
Victor Augusto <augustovictor85@gmail.com>
2023-12-27 01:28:06 -0300

    Build app layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -22,17 +23,19 @@ function App() {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route
-						index
-						element={<Navigate replace to="dashboard" />}
-					/>
-					<Route path="account" element={<Account />} />
-					<Route path="bookings" element={<Bookings />} />
-					<Route path="cabins" element={<Cabins />} />
-					<Route path="dashboard" element={<Dashboard />} />
+					<Route element={<AppLayout />}>
+						<Route
+							index
+							element={<Navigate replace to="dashboard" />}
+						/>
+						<Route path="account" element={<Account />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="users" element={<Users />} />
+					</Route>
 					<Route path="login" element={<Login />} />
-					<Route path="settings" element={<Settings />} />
-					<Route path="users" element={<Users />} />
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
