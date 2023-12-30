d9c5b25f7cdeb9ee41788dd2aa44ff245dc75738
Victor Augusto <augustovictor85@gmail.com>
2023-12-30 08:26:27 -0300

    Handle delete cabin by query mutation

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -12,3 +12,17 @@ export async function getCabins() {
 
 	return data;
 }
+
+export async function deleteCabin(id) {
+	const { data, error } = await supabase
+		.from('cabins')
+		.delete()
+		.eq('id', id);
+
+	if (error) {
+		console.log(error);
+		throw new Error('Cabins could not be deleted');
+	}
+
+	return data;
+}
