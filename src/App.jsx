442134e8a9a002e084c6ab32b99887671dd0417a
Victor Augusto <augustovictor85@gmail.com>
2023-12-30 08:37:54 -0300

    Display notifications on toats and add react-hot-toats package

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Settings from './pages/Settings';
 import Users from './pages/Users';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -54,6 +55,26 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
+			<Toaster
+				position="top-center"
+				gutter={12}
+				containerStyle={{ margin: '8px' }}
+				toastOptions={{
+					success: {
+						duration: 5000,
+					},
+					error: {
+						duration: 9000,
+					},
+					style: {
+						fontSize: '16px',
+						maxWidth: '500px',
+						padding: '16px 24px',
+						backgroundColor: 'var(--color-grey-0)',
+						color: 'var(--color-grey-700)',
+					},
+				}}
+			/>
 		</QueryClientProvider>
 	);
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,6 +6,7 @@ import {
 	useQueryClient,
 } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
 	display: grid;
@@ -61,13 +62,13 @@ function CabinRow({ cabin }) {
 	const { isLoading: isDeleting, mutate } = useMutation({
 		mutationFn: deleteCabin,
 		onSuccess: () => {
-			alert('Cabin successfully deleted');
+			toast.success('Cabin successfully deleted');
 
 			queryClient.invalidateQueries({
 				queryKey: ['cabins'],
 			});
 		},
-		onError: (err) => alert(err.message),
+		onError: (err) => toast.error(err.message),
 	});
 
 	return (
