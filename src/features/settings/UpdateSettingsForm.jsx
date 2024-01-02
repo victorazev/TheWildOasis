51e6b9c3396f26d3c848539611ae78227f9b13c3
Victor Augusto <augustovictor85@gmail.com>
2024-01-02 19:20:21 -0300

    Handle update setting and create useUpdateSetting custom hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
 import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSetting';
 
 function UpdateSettingsForm() {
 	const {
@@ -15,6 +16,14 @@ function UpdateSettingsForm() {
 		} = {},
 	} = useSettings();
 
+	const { isUpdating, updateSetting } = useUpdateSetting();
+
+	function handleUpdate(evt, field) {
+		const { value } = evt.target;
+		if (!value) return;
+		updateSetting({ [field]: value });
+	}
+
 	if (isLoading) return <Spinner />;
 
 	return (
@@ -23,28 +32,42 @@ function UpdateSettingsForm() {
 				<Input
 					type="number"
 					id="min-nights"
+					disabled={isUpdating}
 					defaultValue={minBookingLength}
+					onBlur={(evt) =>
+						handleUpdate(evt, 'minBookingLength')
+					}
 				/>
 			</FormRow>
 			<FormRow label="Maximum nights/booking">
 				<Input
 					type="number"
 					id="max-nights"
+					disabled={isUpdating}
 					defaultValue={maxBookingLength}
+					onBlur={(evt) =>
+						handleUpdate(evt, 'maxBookingLength')
+					}
 				/>
 			</FormRow>
 			<FormRow label="Maximum guests/booking">
 				<Input
 					type="number"
 					id="max-guests"
+					disabled={isUpdating}
 					defaultValue={maxGuestsPerBooking}
+					onBlur={(evt) =>
+						handleUpdate(evt, 'maxGuestsPerBooking')
+					}
 				/>
 			</FormRow>
 			<FormRow label="Breakfast price">
 				<Input
 					type="number"
 					id="breakfast-price"
+					disabled={isUpdating}
 					defaultValue={breakfastPrice}
+					onBlur={(evt) => handleUpdate(evt, 'breakfastPrice')}
 				/>
 			</FormRow>
 		</Form>
