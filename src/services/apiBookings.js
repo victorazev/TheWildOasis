2de567a8a3f30de1de16d3a83f52980957e70c9d
Victor Augusto <augustovictor85@gmail.com>
2024-01-09 02:32:19 -0300

    Add filter to bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-unused-vars */
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-	const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+	let query = supabase
 		.from('bookings')
 		.select(
 			'id, createdAt, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
 		);
+
+	if (filter !== null)
+		query = query[filter.method || 'eq'](
+			filter.field,
+			filter.value,
+		);
+
+	const { data, error } = await query;
 
 	if (error) {
 		console.log(error);
