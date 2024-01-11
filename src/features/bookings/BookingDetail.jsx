8050a565f488b98673708a82704bc40fed239574
Victor Augusto <augustovictor85@gmail.com>
2024-01-11 11:59:49 -0300

    Add check out to bookings

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from './useBooking';
 import Spinner from '../../ui/Spinner';
 import { useNavigate } from 'react-router-dom';
+import { useCheckout } from '../check-in-out/useCheckout';
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
 	const { booking, isLoading } = useBooking();
+	const { checkout, isCheckingOut } = useCheckout();
 
 	const moveBack = useMoveBack();
 
@@ -56,6 +58,15 @@ function BookingDetail() {
 						onClick={() => navigate(`/checkin/${bookingId}`)}
 					>
 						Check in
+					</Button>
+				)}
+
+				{status === 'checked-in' && (
+					<Button
+						onClick={() => checkout(bookingId)}
+						disabled={isCheckingOut}
+					>
+						Check out
 					</Button>
 				)}
 
