4b87fec7b436602bb6264343df68b72e38e109fb
Victor Augusto <augustovictor85@gmail.com>
2024-01-14 14:49:06 -0300

    Build user logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -16,7 +16,7 @@ export function useLogin() {
 
 		onSuccess: (user) => {
 			queryClient.setQueriesData(['user'], user);
-			navigate('/dashboard');
+			navigate('/dashboard', { replace: true });
 		},
 
 		onError: (err) => {
