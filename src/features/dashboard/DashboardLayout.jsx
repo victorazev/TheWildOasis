f54d9423cfdbc12eeb7aae02e87072a49d99fd4b
Victor Augusto <augustovictor85@gmail.com>
2024-01-18 21:44:05 -0300

    Build dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	return (
+		<StyledDashboardLayout>
+			<div>Statistcs</div>
+			<div>Today&apos;s activity</div>
+			<div>Chart stay durations</div>
+			<div>Chart sales</div>
+		</StyledDashboardLayout>
+	);
+}
+
+export default DashboardLayout;
