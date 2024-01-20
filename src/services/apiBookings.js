321359fce104ee8c3adb4c90f0840a8065d49ac4
Victor Augusto <augustovictor85@gmail.com>
2024-01-19 21:05:47 -0300

    Add custom hook to compute recent bookings and stays

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -33,7 +33,7 @@ export async function getBookings({ filter, sortBy, page }) {
 	const { data, error, count } = await query;
 
 	if (error) {
-		console.log(error);
+		console.error(error);
 		throw new Error('Bookings could not be loaded');
 	}
 
@@ -56,12 +56,13 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOString
 export async function getBookingsAfterDate(date) {
 	const { data, error } = await supabase
 		.from('bookings')
-		.select('created_at, totalPrice, extrasPrice')
-		.gte('created_at', date)
-		.lte('created_at', getToday({ end: true }));
+		.select('createdAt, totalPrice, extrasPrice')
+		.gte('createdAt', date)
+		.lte('createdAt', getToday({ end: true }));
 
 	if (error) {
 		console.error(error);
@@ -75,7 +76,6 @@ export async function getBookingsAfterDate(date) {
 export async function getStaysAfterDate(date) {
 	const { data, error } = await supabase
 		.from('bookings')
-		// .select('*')
 		.select('*, guests(fullName)')
 		.gte('startDate', date)
 		.lte('startDate', getToday());
@@ -96,7 +96,7 @@ export async function getStaysTodayActivity() {
 		.or(
 			`and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`,
 		)
-		.order('created_at');
+		.order('createdAt');
 
 	// Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
 	// (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
