ed4034427a3d2e2c66b259b3cb2b85e235a8609f
Victor Augusto <augustovictor85@gmail.com>
2024-01-21 20:32:27 -0300

    Display stays for current day

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,21 @@
-import Button from "../../ui/Button";
+/* eslint-disable react/prop-types */
+import { useCheckout } from './useCheckout';
+
+import Button from '../../ui/Button';
 
 function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
+	const { checkout, isCheckingOut } = useCheckout();
+
+	return (
+		<Button
+			variation="primary"
+			size="small"
+			onClick={() => checkout(bookingId)}
+			disabled={isCheckingOut}
+		>
+			Check out
+		</Button>
+	);
 }
 
 export default CheckoutButton;
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -8,6 +8,7 @@ import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -37,8 +38,11 @@ function DashboardLayout() {
 				numDays={numDays}
 				cabinCount={cabins.length}
 			/>
-			<div>Today&apos;s activity</div>
+
+			<TodayActivity />
+
 			<DurationChart confirmedStays={confirmedStays} />
+
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	);
