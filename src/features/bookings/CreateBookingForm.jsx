1d20ebd86b53a4092a489b8b64b148812831a368
Victor Augusto <augustovictor85@gmail.com>
2024-02-01 00:45:45 -0300

    Improve front: Add numGuests and hasBreakfast to form

--- src/features/bookings/CreateBookingForm.jsx
+++ src/features/bookings/CreateBookingForm.jsx
@@ -1,35 +1,52 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { differenceInBusinessDays } from 'date-fns';
+import { differenceInDays } from 'date-fns';
 
 import { useGuests } from '../guests/useGuests';
 import { useCabins } from '../cabins/useCabins';
 import { useCreateBooking } from './useCreateBooking';
+import { useSettings } from '../settings/useSettings';
 
+import { formatCurrency } from '../../utils/helpers';
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Button from '../../ui/Button';
 import Spinner from '../../ui/Spinner';
 import Select from '../../ui/Select';
 import DatePickerRow from '../../ui/DatePickerRow';
+import Checkbox from '../../ui/Checkbox';
 
 function CreateBookingForm({ onCloseModal }) {
+	const [options, setOptions] = useState([]);
+	const [isSelectedCabin, setIsSelectedCabin] = useState(false);
+	const [numGuests, setNumGuests] = useState('');
+
 	const { guests, isLoading: isLoadingGuests } = useGuests();
 
 	const { cabins, isLoading: isLoadingCabins } = useCabins();
 
+	const { settings, isLoading: isLoadingSettings } =
+		useSettings();
+
 	const { createBooking, isCreating } = useCreateBooking();
 
-	const { handleSubmit, control } = useForm({
-		defaultValues: {
-			guestId: '',
-			cabinId: '',
-		},
-	});
+	const { handleSubmit, control, setValue, getValues } =
+		useForm({
+			defaultValues: {
+				guestId: '',
+				cabinId: '',
+				numGuests: '',
+				startDate: null,
+				endDate: null,
+			},
+		});
 
-	if (isLoadingGuests || isLoadingCabins) return <Spinner />;
+	if (isLoadingGuests || isLoadingCabins || isLoadingSettings)
+		return <Spinner />;
 
 	const guestOptions = guests.map((guest) => ({
 		value: guest.id,
@@ -41,8 +58,37 @@ function CreateBookingForm({ onCloseModal }) {
 		label: cabin.name,
 	}));
 
+	function handleChangeCabin(evt) {
+		setValue('cabinId', evt.target.value);
+
+		let numGuests = [];
+
+		const selectedCabin = cabins.filter(
+			(cabin) => cabin.id == evt.target.value,
+		);
+		const maxNumGuests = selectedCabin[0].maxCapacity;
+		const minGuests = Math.ceil(maxNumGuests / 2);
+
+		for (let i = minGuests; i <= maxNumGuests; i++) {
+			numGuests.push(i);
+		}
+
+		const totalGuestOptions = numGuests.map((num) => ({
+			value: num,
+			label: num,
+		}));
+
+		setOptions(totalGuestOptions);
+		setIsSelectedCabin(true);
+	}
+
+	function handleChangeGuests(evt) {
+		setValue('numGuests', evt.target.value);
+		setNumGuests(evt.target.value);
+	}
+
 	function onSubmit(data) {
-		const numNights = differenceInBusinessDays(
+		const numNights = differenceInDays(
 			data.endDate,
 			data.startDate,
 		);
@@ -51,25 +97,26 @@ function CreateBookingForm({ onCloseModal }) {
 			(cabin) => cabin.id == data.cabinId,
 		);
 
-		const numGuests = selectedCabin[0].maxCapacity;
-
 		const cabinPrice =
 			numNights * selectedCabin[0].regularPrice -
 			numNights * selectedCabin[0].discount;
 
-		const hasBreakfast = false;
-		const extrasPrice = 0;
+		const extrasPrice =
+			differenceInDays(
+				getValues('endDate'),
+				getValues('startDate'),
+			) *
+			numGuests *
+			settings.breakfastPrice;
 
 		const totalPrice = cabinPrice + extrasPrice;
 
 		const newBooking = {
 			...data,
-			numGuests,
 			totalPrice,
 			cabinPrice,
 			numNights,
 			extrasPrice,
-			hasBreakfast,
 			status: 'unconfirmed',
 			isPaid: false,
 			observations: '',
@@ -105,8 +152,30 @@ function CreateBookingForm({ onCloseModal }) {
 							options={cabinOptions}
 							value={value}
 							type="white"
-							onChange={onChange}
+							onChange={(evt) => {
+								handleChangeCabin(evt);
+								onChange;
+							}}
 							disabled={isCreating}
+						/>
+					)}
+				/>
+			</FormRow>
+
+			<FormRow label="Total of guests">
+				<Controller
+					name="numGuests"
+					control={control}
+					render={({ field: { onChange, value } }) => (
+						<Select
+							options={options}
+							value={value}
+							type="white"
+							onChange={(evt) => {
+								handleChangeGuests(evt);
+								onChange;
+							}}
+							disabled={isCreating || !isSelectedCabin}
 						/>
 					)}
 				/>
@@ -145,6 +214,38 @@ function CreateBookingForm({ onCloseModal }) {
 					)}
 				/>
 			</DatePickerRow>
+
+			<FormRow label="Breakfast">
+				<Controller
+					name="hasBreakfast"
+					control={control}
+					render={({ field: { onChange, value } }) => (
+						<Checkbox
+							checked={value}
+							onChange={onChange}
+							disabled={isCreating}
+						>
+							<p>
+								Add breakfast for{' '}
+								{formatCurrency(settings.breakfastPrice)} per
+								guest?
+								{numGuests != '' &&
+								getValues('endDate') != null &&
+								getValues('startDate') != null
+									? ` Total of ${formatCurrency(
+											differenceInDays(
+												getValues('endDate'),
+												getValues('startDate'),
+											) *
+												numGuests *
+												settings.breakfastPrice,
+									  )}`
+									: ''}
+							</p>
+						</Checkbox>
+					)}
+				/>
+			</FormRow>
 
 			<FormRow>
 				<Button
