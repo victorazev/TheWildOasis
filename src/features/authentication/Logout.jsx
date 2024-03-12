f66f87cc58da16ae1e9239a2e6e19d151b0af127
Victor Augusto <augustovictor85@gmail.com>
2024-03-12 12:52:30 -0300

    refactor(logout): change folder

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,6 +1,6 @@
 import { HiArrowRightOnRectangle } from 'react-icons/hi2';
 import ButtonIcon from '../../ui/ButtonIcon';
-import { useLogout } from '../cabins/useLogout';
+import { useLogout } from './useLogout';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function Logout() {
